806eb46d3a21d62c7c5453974484d6da00cbd303
Reynaldo Sánchez <50810136+ReynaldoSQ@users.noreply.github.com>
2020-06-16 00:33:52 -0700

    ee
    ddw

--- server.py
+++ server.py
@@ -1,5 +1,7 @@
+
 export_file_url='https://drive.google.com/file/d/1O7n54pQbwjpRVajeukeQWVHEsXq9wCtf/view?usp=sharing'
 export_file_name='export.pkl'
 
 classes=['ductal','lobular']
 path= Path(__file__).parent
+
