b8ee669b38eebdc2d2c37d18594c4b233d2d46be
Reynaldo Sánchez <50810136+ReynaldoSQ@users.noreply.github.com>
2020-06-16 00:24:44 -0700

    Update server.py

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-export_file_url=''
+export_file_url='https://drive.google.com/file/d/1O7n54pQbwjpRVajeukeQWVHEsXq9wCtf/view?usp=sharing'
 export_file_name='export.pkl'
 
 classes=['ductal','lobular']
